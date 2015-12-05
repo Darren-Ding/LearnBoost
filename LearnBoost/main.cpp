0066f428215eb4f7967ec323ccada52976ec22c5
Darren-Ding <darrencoder@163.com>
2015-12-06 00:14:38 +0800

    Start Boost Learning.

--- LearnBoost/main.cpp
+++ LearnBoost/main.cpp
@@ -6,10 +6,23 @@
 //  Copyright © 2015年  DingLixiang. All rights reserved.
 //
 
-#include <iostream>
+/** Boost Learning Journey Start! **/
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+#include <iostream>
+#include <boost/version.hpp>
+#include <boost/config.hpp>
+
+int main(int argc, const char * argv[])
+{
     std::cout << "Hello, World!\n";
+    std::cout << "Hello, Boost!\n";
+    
+    std::cout << "BOOST_VERSION: " << BOOST_VERSION << std::endl;
+    std::cout << "BOOST_LIB_VERSION: " << BOOST_LIB_VERSION << std::endl;
+    
+    std::cout << "BOOST_PLATFORM: " << BOOST_PLATFORM << std::endl;
+    std::cout << "BOOST_COMPILER: " << BOOST_COMPILER << std::endl;
+    std::cout << "BOOST_STDLIB: " << BOOST_STDLIB << std::endl;
+    
     return 0;
 }
