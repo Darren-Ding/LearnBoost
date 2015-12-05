4c60e1144d56926363470d2ed9af899d21d55fd9
Darren-Ding <darrencoder@163.com>
2015-12-06 00:09:56 +0800

    Initial Commit

---
+++ LearnBoost/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnBoost
+//
+//  Created by  DingLixiang on 15/12/6.
+//  Copyright © 2015年  DingLixiang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
